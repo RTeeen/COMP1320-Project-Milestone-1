7c44d01c0d3c31d69eb5722b35cee8a7f709a0b0
RTeeen <71365180+RTeeen@users.noreply.github.com>
2020-11-12 21:09:51 -0800

    Program done

--- IOhandler.js
+++ IOhandler.js
@@ -3,15 +3,17 @@
  * File Name: IOhandler.js
  * Description: Collection of functions for files input/output related operations
  * 
- * Created Date: 
- * Author: 
+ * Created Date: November 8th 2020
+ * Author: Artin Peyvasteh
  * 
  */
 
 const unzipper = require('unzipper'),
-  fs = require("fs"),
+  fs = require('fs'),
+  fsP = fs.promises,
   PNG = require('pngjs').PNG,
   path = require('path');
+  
 
 
 /**
@@ -21,7 +23,23 @@ const unzipper = require('unzipper'),
  * @param {string} pathOut 
  * @return {promise}
  */
+
 const unzip = (pathIn, pathOut) => {
+  return new Promise((resolve,reject)=>{
+    fs.createReadStream(pathIn)
+      .pipe(
+        unzipper.Extract({ path: pathOut})
+      )
+      .on("finish",function(){
+
+        resolve("Extraction Completed");
+      })
+      .on("error", function(){
+        reject("There was an error while extracting the file!");
+      })
+    
+      
+  });
 
 };
 
@@ -30,9 +48,24 @@ const unzip = (pathIn, pathOut) => {
  * 
  * @param {string} path 
  * @return {promise}
+ * 
+ * 
  */
-const readDir = dir => {
+let pngFiles =[];
+let pathFiles =[];
 
+const readDir = dir => {
+return new Promise((resolve, reject)=>{
+  fsP.readdir(dir, "utf8")
+    .then((data)=> {
+      pngFiles = data.filter((file)=> path.extname(file).toLowerCase() == ".png")
+      pngFiles.forEach(element => pathFiles.push(path.resolve(`./unzipped/${element}`)))
+
+      resolve(pathFiles);
+    })
+    .catch((err)=> reject(err))
+
+});
 };
 
 /**
@@ -42,9 +75,40 @@ const readDir = dir => {
  * @param {string} filePath 
  * @param {string} pathProcessed 
  * @return {promise}
+ * 
  */
+let average,x=0,R=0,G=1,B=2;
 const grayScale = (pathIn, pathOut) => {
-
+  return new Promise((resolve,reject)=>{
+    pathIn.forEach(element => {
+      fs.createReadStream(element)
+      .pipe(new PNG())
+      .on("parsed",function(data){
+        let fileName= path.win32.basename(element);
+        while(x<=(this.width*this.height)){
+          average= (this.data[R]+this.data[G]+this.data[B])/3;
+          this.data[R]= average;
+          this.data[G]= average;
+          this.data[B]= average;
+          R=R+4;
+          G=G+4;
+          B=B+4;
+          x++;
+        }
+        R=0;
+        G=1;
+        B=2;
+        x=0;
+        this.pack().pipe(fs.createWriteStream(`${pathOut}/GS_${fileName}`));
+        resolve("Grayscale filter applied to all the photos in the directory!");
+      })
+      .on("error",(error)=>{
+        reject(error);
+      })
+      
+    });
+    
+  });
 };
 
 module.exports = {
@@ -52,3 +116,4 @@ module.exports = {
   readDir,
   grayScale
 };
+
--- main.js
+++ main.js
@@ -1,16 +1,25 @@
 /*
  * Project: COMP1320 Milestone 1
  * File Name: main.js
- * Description: 
+ * Description: Calling all of our modules in the main file.
  * 
- * Created Date: 
- * Author:
+ * Created Date: Nov 8th, 2020
+ * Author: Artin Peyvasteh
  * 
  */
 
-const IOhandler = require("./IOhandler"),
+const {unzip, readDir,grayScale} = require("./IOhandler"),
   zipFilePath = `${__dirname}/myfile.zip`,
   pathUnzipped = `${__dirname}/unzipped`,
   pathProcessed = `${__dirname}/grayscaled`;
 
-
+  unzip(zipFilePath,pathUnzipped)
+   .then((msg)=>{
+      console.log(msg);
+      return readDir(pathUnzipped);
+    })
+    .then((result)=>{
+      return grayScale(result,pathProcessed);
+    })
+    .then((msg)=> console.log(msg))
+    .catch((err)=> console.log(err))
